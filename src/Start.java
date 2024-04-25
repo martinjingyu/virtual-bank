659d6c071712311c8dd1c8aa5c7ce44e4da536c4
JINGYU HUANG <106376321+martinjingyu@users.noreply.github.com>
2024-04-25 17:49:15 +0800

    Delete src directory

--- src/Start.java
+++
@@ -1,9 +0,0 @@
-
-// [pwd]:4/25：作为启动文件，用于启动整个项目
-
-public class Start {
-    public static void main(String[] args) {
-        GUI.log_in.GUIMain ui = new GUI.log_in.GUIMain();
-        ui.createAndShowGUI();
-    }
-}
