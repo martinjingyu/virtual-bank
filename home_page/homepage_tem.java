51e4c2707e4bf2f0d39c31a96b88bf304acd555b
wunon77 <134582257+wunon77@users.noreply.github.com>
2024-05-02 10:52:23 +0800

    Add files via upload

---
+++ home_page/homepage_tem.java
@@ -0,0 +1,10 @@
+package GUI.home_page;
+import javax.swing.*;
+
+public class homepage_tem extends JPanel{
+    private  JLabel text;
+    public homepage_tem(){
+        text = new JLabel("this is templete_homepage");
+        add(text);
+    }
+}
