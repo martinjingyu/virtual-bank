df5e176cb91dbac8e6540ccf48c05472ae99d485
RollingRo66 <154528803+RollingRo66@users.noreply.github.com>
2024-05-07 18:50:02 +0800

    Add files via upload

---
+++ src/Entity/Kids.java
@@ -0,0 +1,36 @@
+package Entity;
+
+public class Kids {
+    private Bank bank;
+    private ProductList productList;
+    private TaskList taskList;
+    private HistoryTransactionList transactionList;
+    private MessageList messagelist;
+
+    public Kids(){
+
+    }
+
+    public Kids(Bank bank,HistoryTransactionList transactionList,
+                ProductList productList, TaskList taskList,MessageList messagelist){
+        this.bank = bank;
+        this.transactionList = transactionList;
+        this.productList = productList;
+        this.taskList = taskList;
+        this.messagelist = messagelist;
+        this.getMessagelist().setKids(this);
+    }
+
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
+    public MessageList getMessagelist() {
+        return messagelist;
+    }
+}
--- src/Entity/MessageList.java
+++ src/Entity/MessageList.java
@@ -5,9 +5,16 @@ import java.util.List;
 
 public class MessageList {
     private List<Message> messages;
+    private Kids kid;
+
 
     public MessageList() {
+
         this.messages = new ArrayList<>();
+    }
+
+    public void setKids(Kids kid) {
+        this.kid = kid;
     }
 
     public void addMessage(Message message) {
@@ -20,6 +27,43 @@ public class MessageList {
 
     public void setMessages(List<Message> messages) {
         this.messages = messages;
+    }
+
+    public List<Message> getParentKidMessages() {
+        List<Message> parentMessages = new ArrayList<>();
+        for (Message message : messages) {
+            if (message.getSender().equals("parent")||message.getSender().equals("kid")) {
+                parentMessages.add(message);
+            }
+        }
+        return parentMessages;
+    }
+    public List<Message> getSystemMessages() {
+        List<Message> parentMessages = new ArrayList<>();
+        for (Message message : messages) {
+            if (message.getSender().equals("system")) {
+                parentMessages.add(message);
+            }
+        }
+        return parentMessages;
+    }
+
+    // Adding new methods to handle different types of messages
+    public void addTaskMessage(Task task, String description) {
+        Message taskMessage = new Message("parent",description);
+        messages.add(taskMessage);
+    }
+
+    public void addBankMessage(Bank bank, double currentInterestRate,double savingInterestRate) {
+        String changeCurrent = String.format("Attention! Your current interest rate has changed to %d",currentInterestRate);
+        Message bankMessage = new Message("parent",changeCurrent);
+        messages.add(bankMessage);
+    }
+
+    public void addShopMessage(double price) {
+        String bigTransaction = String.format("Attention! You made a large expenditure, spending %d.", price);
+        Message shopMessage = new Message("system",bigTransaction);
+        messages.add(shopMessage);
     }
 
 
