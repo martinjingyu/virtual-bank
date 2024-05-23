df4a3d5c909efd72494ef22fa0df5ece12053537
Jacobi120 <2109467620@qq.com>
2024-05-23 17:00:50 +0800

    Add files via upload

--- src/utill/read/CheckID.java
+++ src/utill/read/CheckID.java
@@ -7,10 +7,9 @@
  */
 package utill.read;
 
-import java.io.File;
-import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Scanner;
-
+import utill.cryption.*;
 import utill.validation.Validate;
 
 /**
@@ -29,17 +28,31 @@ public class CheckID {
      */
     public static boolean checkID(String text1, String filePath) {
         try {
-
-            File file = new File(filePath);
-            Scanner scanner = new Scanner(file);
+            String content = decryptFileContents(filePath);
+            // Create Scanner objects to read file contents
+            Scanner scanner = new Scanner(content);
             return Validate.checkid(scanner, text1);
 
-        } catch (IOException error) {
+        } catch (Exception error) {
             System.out.println("error_id");
             System.exit(1);
             return false;
         }
 
+    }
+
+    /**
+     * Reads the contents of a file, decrypts the content using an EncryptionUtil
+     * class, and returns it as a string.
+     *
+     * @param filePath The path of the file to be decrypted.
+     * @return The decrypted file contents as a string.
+     * @throws Exception If there is an error while reading or decrypting the file.
+     */
+    public static String decryptFileContents(String filePath) throws Exception {
+        byte[] fileContent = Files.readAllBytes(java.nio.file.Paths.get(filePath));
+        return EncryptionUtil.decrypt(new String(fileContent, java.nio.charset.StandardCharsets.UTF_8)); // re //
+                                                                                                         // string.
     }
 
 }
